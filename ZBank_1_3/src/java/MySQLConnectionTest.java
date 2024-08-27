3162dc8d02b4a6c94f35d2797cc2d5e32e255126
Oguzhan Yigit <147761081+Oguzhanyigitt@users.noreply.github.com>
2024-08-27 09:14:40 +0300

    Update MySQLConnectionTest.java

--- ZBank_1_3/src/java/MySQLConnectionTest.java
+++ ZBank_1_3/src/java/MySQLConnectionTest.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author db2admin
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
